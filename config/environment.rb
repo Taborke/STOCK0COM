7a767adaa3804b16c4f7534d0d0fde040f1bd690
taborke <kevin.tabor@gmail.com>
2014-06-10 13:21:47 -0500

    Mongoid

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,19 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require 'mongo'
-require 'uri'
+# require 'mongo'
+# require 'uri'
 
-def get_connection
-  return @db_connection if @db_connection
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  @db_connection
-end
+# def get_connection
+#   return @db_connection if @db_connection
+#   db = URI.parse(ENV['MONGOHQ_URL'])
+#   db_name = db.path.gsub(/^\//, '')
+#   @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+#   @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+#   @db_connection
+# end
 
-db = get_connection
+# db = get_connection
 
 # Initialize the Rails application.
 Rails.application.initialize!
