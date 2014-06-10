79af21d38644244618237ddbd1106939a88851ca
taborke <kevin.tabor@gmail.com>
2014-06-10 13:14:12 -0500

    Mongo DB

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require 'mongo'
+require 'uri'
+
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
+
+db = get_connection
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-require 'mongo'
-require 'uri'
 
-def get_connection
-  return @db_connection if @db_connection
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  @db_connection
-end
-
-db = get_connection
   # Code is not reloaded between requests.
   config.cache_classes = true
 
