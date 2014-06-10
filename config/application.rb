bae0df86aa810336cc3ec9f2667d8f076036f857
taborke <kevin.tabor@gmail.com>
2014-06-10 12:11:42 -0500

    Mongo

--- config/application.rb
+++ config/application.rb
@@ -13,21 +13,6 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
-require 'mongo'
-require 'uri'
-
-def get_connection
-  return @db_connection if @db_connection
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  @db_connection
-end
-
-db = get_connection
-
 module STOCK0COM
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
