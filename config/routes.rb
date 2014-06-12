41081a42b492a7c1b03cd7700da1703af527e2a9
taborke <kevin.tabor@gmail.com>
2014-06-12 15:21:34 -0500

    Adding Stock Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-
+  
+  get 'stocks/show'
+  resources :stocks do
+      resources :stock_histories
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
