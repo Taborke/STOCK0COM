ac134f77cbcec49736989a41809f0b70740a73c1
taborke <kevin.tabor@gmail.com>
2014-06-12 16:15:03 -0500

    Adding Images

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,19 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-  
+  default from: "SCHTOCK0COM"
+
   def welcome_email(user)
   	@user = user
   	@url = 'http://glacial-eyrie-4756.herokuapp.com'
   	mail(to: @user.email, subject: 'Welcome to SCHTOCK0COM')
+  end
+
+  def dist_day_email(days)
+     #send to all users
+     @dist_days = days #array of stock histories with dist days
+     @stock = @dist_days.last.stock
+     @stock = @stock.name
+     @day_count = @dist_days.length #variable for dist day count
+     @emails = Users.all.email
+     mail(bcc: @emails, subject: "#{@stock} has had a distribution day")
   end
 end
