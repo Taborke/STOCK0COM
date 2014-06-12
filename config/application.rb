b0f22ca5ed191fa56fa87907864ccda11049bd4c
taborke <kevin.tabor@gmail.com>
2014-06-12 08:17:52 -0500

    Adding require YF

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
-
+require 'yahoo_finance'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
