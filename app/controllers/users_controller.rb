a5d8fe194489307fbaafb33ac440c54da4d55b4f
taborke <kevin.tabor@gmail.com>
2014-06-30 17:50:03 -0500

    mail after_action create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  after_create :mail_out
+  after_action :mail_out, only: [:create]
   # GET /users
   # GET /users.json
   def index
