35d40682c24407829ee9f0a969a386ad56cdd558
taborke <kevin.tabor@gmail.com>
2014-07-10 19:20:45 -0500

    Read me

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,13 @@ class WelcomeController < ApplicationController
 	def index
 		
 		t = Time.now #this will tell you if the NYSE is still currently open
-		if (t.hour > 21 || t.hour < 14) || (t.wday == 0) || (t.wday == 6)
-			@market_closed = "Closed" 
+		if (t.hour > 21 || (t.hour < 14 && t.min >= 30)) || (t.wday == 0) || (t.wday == 6)
+			@market_closed = "Closed"	
 		else
 			@market_closed = "Open"
 		end
 		@stocks = []
-        @dday = "black"
         Stock.all.each do |stock|
-
         last_stock = StockHistory.where(stock: stock).asc(:trade_date).last
         unless last_stock.nil?
           @stocks.push(last_stock)
